5695ae474b74fad1861edf5c8457b05972fd3755
Chen Yu Pao <windperson@gmail.com>
2018-11-04 17:36:29 +0800

    Using SKGLView to draw a circle

--- SkiaSharpGL_demo/SkiaSharpGL_demo/MainPage.xaml.cs
+++ SkiaSharpGL_demo/SkiaSharpGL_demo/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 
 namespace SkiaSharpGL_demo
@@ -12,6 +14,42 @@ namespace SkiaSharpGL_demo
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void SKGLView_OnPaintSurface(object sender, SKPaintGLSurfaceEventArgs e)
+        {
+            var canvas = e.Surface.Canvas;
+
+            canvas.Clear(SKColors.White);
+
+            DrawCircle(canvas);
+        }
+
+        private void DrawCircle(SKCanvas canvas)
+        {
+            using (var circleFill = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Blue
+            })
+            {
+                // draw the circle fill
+                canvas.DrawCircle(100, 100, 40, circleFill);
+            }
+
+            // create the paint for the circle border
+            using (var circleBorder = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Red,
+                StrokeWidth = 5
+            })
+            {
+                // draw the circle border
+                canvas.DrawCircle(100, 100, 40, circleBorder);
+            }
         }
     }
 }
