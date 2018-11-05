70fb9abcee3ce5f054b1d8236ddd85cc061430a4
Chen Yu Pao <windperson@gmail.com>
2018-11-05 11:02:04 +0800

    Animation example from: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/graphics/skiasharp/basics/animation

--- SkiaSharpGL_demo/SkiaSharpGL_demo/MainPage.xaml.cs
+++ SkiaSharpGL_demo/SkiaSharpGL_demo/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,45 +12,69 @@ namespace SkiaSharpGL_demo
 {
     public partial class MainPage : ContentPage
     {
+        Stopwatch stopwatch = new Stopwatch();
+        bool pageIsActive;
+        float scale;
+
         public MainPage()
         {
             InitializeComponent();
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            pageIsActive = true;
+            AnimationLoop();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            pageIsActive = false;
+        }
+
+        async Task AnimationLoop()
+        {
+            stopwatch.Start();
+
+            while (pageIsActive)
+            {
+                double cycleTime = slider.Value;
+                double t = stopwatch.Elapsed.TotalSeconds % cycleTime / cycleTime;
+                scale = (1 + (float)Math.Sin(2 * Math.PI * t)) / 2;
+                canvasView.InvalidateSurface();
+                await Task.Delay(TimeSpan.FromSeconds(1.0 / 30));
+            }
+            stopwatch.Stop();
+        }
+
         private void SKGLView_OnPaintSurface(object sender, SKPaintGLSurfaceEventArgs e)
         {
-            var canvas = e.Surface.Canvas;
 
+            var canvas = e.Surface.Canvas;
             canvas.Clear(SKColors.White);
 
-            DrawCircle(canvas);
-        }
+            var info = e.RenderTarget;
 
-        private void DrawCircle(SKCanvas canvas)
-        {
-            using (var circleFill = new SKPaint
-            {
-                IsAntialias = true,
-                Style = SKPaintStyle.Fill,
-                Color = SKColors.Blue
-            })
-            {
-                // draw the circle fill
-                canvas.DrawCircle(100, 100, 40, circleFill);
-            }
+            var maxRadius = 0.75f * Math.Min(info.Width, info.Height) / 2;
+            var minRadius = 0.25f * maxRadius;
 
-            // create the paint for the circle border
-            using (var circleBorder = new SKPaint
+            var xRadius = minRadius * scale + maxRadius * (1 - scale);
+            var yRadius = maxRadius * scale + minRadius * (1 - scale);
+
+            using (var paint = new SKPaint())
             {
-                IsAntialias = true,
-                Style = SKPaintStyle.Stroke,
-                Color = SKColors.Red,
-                StrokeWidth = 5
-            })
-            {
-                // draw the circle border
-                canvas.DrawCircle(100, 100, 40, circleBorder);
+                paint.Style = SKPaintStyle.Stroke;
+                paint.Color = SKColors.Blue;
+                paint.StrokeWidth = 50;
+                canvas.DrawOval(info.Width / 2.0f, info.Height / 2.0f, xRadius, yRadius, paint);
+
+                paint.Style = SKPaintStyle.Fill;
+                paint.Color = SKColors.SkyBlue;
+                canvas.DrawOval(info.Width / 2.0f, info.Height / 2.0f, xRadius, yRadius, paint);
             }
         }
+
     }
 }
